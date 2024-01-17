a2ac2e1a61159012144ee68af9ae01b95bd686b2
kelvinsekx <utdkelvin@gmail.com>
2024-01-17 16:04:45 +0000

    adjust container width

--- components/Container.tsx
+++ components/Container.tsx
@@ -3,5 +3,5 @@ import React, { ComponentPropsWithoutRef } from "react";
 export const Container: React.FC<ComponentPropsWithoutRef<"div">> = ({
   children,
 }) => {
-  return <div className="container w-[70%] mx-auto ">{children}</div>;
+  return <div className="container md:w-[70%] mx-auto ">{children}</div>;
 };
