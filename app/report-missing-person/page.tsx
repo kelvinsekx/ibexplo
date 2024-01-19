53858fecff23fe6e7fb4d3f93d346db27888ce96
kelvinsekx <utdkelvin@gmail.com>
2024-01-19 13:12:16 +0000

    cleaning types

--- app/report-missing-person/page.tsx
+++ app/report-missing-person/page.tsx
@@ -55,7 +55,7 @@ const formSchema = z.object({
     )
     .refine(
       (files) =>
-        Array.from(files).every((file) =>
+        Array.from(files).every((file as File) =>
           ACCEPTED_IMAGE_TYPES.includes(file.type),
         ),
       "Only these types are allowed .jpg, .jpeg, .png and .webp",
