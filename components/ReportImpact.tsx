25e547c3571dc96d7c8a862e4fbac58b9dc0eb12
kelvinsekx <utdkelvin@gmail.com>
2024-01-22 14:46:42 +0000

    still in progress4

--- components/ReportImpact.tsx
+++ components/ReportImpact.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 
-export const ReportImpactBtn = ({ type }: { type }) => (
+export const ReportImpactBtn = ({ type }: { type: string }) => (
   <Link href={"/" + type + "/report-impact"}>
     <Button>Report your impact ❤️</Button>
   </Link>
