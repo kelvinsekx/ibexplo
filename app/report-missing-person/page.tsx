291e42f13cc582adf675b9240f3c103599b7decb
kelvinsekx <utdkelvin@gmail.com>
2024-01-22 14:45:00 +0000

    still in progress3

--- app/report-missing-person/page.tsx
+++ app/report-missing-person/page.tsx
@@ -91,7 +91,7 @@ export default function ReportMissingPerson({
       <div className="space-y-[32px]">
         <ProfileForm type={searchParams.type} />
         <footer className="text-center pt-40 flex flex-col justify-center">
-          <ReportImpactBtn />
+          <ReportImpactBtn type={searchParams.type} />
           <small className="text-xs pt-10">
             Updates are real time, Last Website Update : 10:07AM
           </small>
